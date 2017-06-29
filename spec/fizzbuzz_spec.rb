5921193b0b925d4dfe4b200e371f221ca44148d5
Toby Hamand <tobyhamand@gmail.com>
2017-06-29 20:22:59 +0100

    Fizzbuzz completed with rspec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
+  end
+  it 'returns the original number when passed any other number' do
+    expect(fizzbuzz(4)).to eq 4
   end
 end
